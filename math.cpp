ff158cc1390fa75c158071002614287b1d9d3293
dygee <dygee2010@hotmail.com>
2018-04-05 22:40:44 +0800

    Add a new variable

--- math.cpp
+++ math.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int add(int num1,int num2);
+int add(int num1,int num2, num3);
 
 int main()
 {
-    int sum = add(10,50);
+    int sum = add(10,50,100);
     cout << "The result of addition is: " << sum;
 }
 
-int add(int num1,int num2)
+int add(int num1,int num2,int num3)
 {
-    int result = num1 + num2;
+    int result = num1 + num2 + num3;
     return result;
 }
